35663e24d887a39555f3e98279d6614faaed030d
Julia Greenwald <julia.greenwald@gmail.com>
2017-02-28 14:49:57 -0800

    add puppy features in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#Puppy
+
+#Breed
+#Corgi
+#German Shepard
+#Husky 
+
+#Characteristics/Behavior
+
+#Corgi
+#Cute, intelligent, independent
+
+#German Shepard
+#Brave, intelligent, good for tracking
+
+#Husky
+#Brave, loves cold weather, tough
