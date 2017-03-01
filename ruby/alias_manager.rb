dda795b41e3c1679eaf9ba11eef94fec854bd59e
juliamji <julia.greenwald@gmail.com>
2017-02-28 18:44:17 -0800

    update code for review

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,30 +1,30 @@
+#notes: i've included a method that i tried to pseudocode out and an alternative algorithm because i'm a bit stuck
+
 #set up method for reordering the name
 #use first name and last name in order to reverse the order 
 #make new variable and concat the name in reverse order
 #now i want to iterate over the reversed name
 #use chars, map, and next 
 #i should have vowels and consonants also set up in array/hash
-#.downcase can be used to ensure the name is properly handled 
-
-#notes: i've included a method that i tried to pseudocode out and an alternative algorithm because i'm a bit stuck 		
+#if/else conditional to 
+#.downcase can be used to ensure the name is properly handled  		
 	
-# def fake_name (first_name, last_name)
+# def reverse_name(first_name, last_name)
 # 	new_arr = []
-# 	reverse = "#{last_name}" + " " + "#{first_name}"
+# 	reverse = "#{last_name}".downcase + " " + "#{first_name}".downcase
 # 	p reverse
 
-# 	#now i want to iterate over the reversed name
-
-# 	vowel= ["a", "e", "i", "o", "u"]
+# 	# if reverse.include?
+# 	vowel = ["a", "e", "i", "o", "u"]
 # 	consonant = []
 
 # 	new_name = reverse.chars.map{ |vowel| vowel.next }
 
-	
-
 # 	p new_name
 	
 # end 
+
+# reverse_name("felicia", "torres")
 
 def fake_name(first_name, last_name)
 	name_arr = []
@@ -56,4 +56,3 @@ fake_name("felicia", "torres")
 # 	full_name = first_name.prepend ("#{last_name} ")
 
 # 	p full_name
-# end	
