60d77c9700650e71ff32d14f507db0a795a41394
Julia Greenwald <julia.greenwald@gmail.com>
2017-03-04 17:11:13 -0800

    add man and woman class to shout module with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,39 @@
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#   	"haha" + words + "~~~" + " :)"
+#   end
+
+# end
+
+# Shout.yell_angrily("i'm so angry")
+# Shout.yell_happily("i'm so angry")
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+	def yell_angrily(words)
+    puts "#{words}" + "!!!" + " :("
+  	end
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+  	def yell_happily(words)
+  	puts "lol " + "#{words}" + "~~~" + " :)"
+  	end
+end 
 
-  def self.yell_happily(words)
-  	"haha" + words + "~~~" + " :)"
-  end
-
+class Man
+	include Shout
 end
 
-Shout.yell_angrily("i'm so angry")
-Shout.yell_happily("i'm so angry")
+class Woman
+	include Shout
+end 
+
+man = Man.new
+man.yell_angrily("i'm so angry")
+
+woman = Woman.new
+woman.yell_happily("i'm so angry")
