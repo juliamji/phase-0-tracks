153011dd3d571c166e4b494f518b107b0ac96136
Julia Greenwald <julia.greenwald@gmail.com>
2017-03-05 15:46:20 -0800

    add pseudocode for game design

---
+++ ruby/wordgame/wordgame.rb
@@ -0,0 +1,7 @@
+#pseudocode
+#we need a game class with several methods
+#something to capture the secret word
+#split or do chars on the word and have that stored
+#if the letter guessed matches a character, print that character in its proper position to the screen
+#if the guess does not match a character, decease/decrement the amount of turns by 1
+#number of guesses should correspond to the word's length 
