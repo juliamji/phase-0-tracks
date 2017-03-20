6da61d06212218356b29ff4eb688f9b9fd19b024
Julia Greenwald <julia.greenwald@gmail.com>
2017-03-19 23:50:54 -0700

    create and add members into the db

--- databases/awesome/awesome.rb
+++ databases/awesome/awesome.rb
@@ -4,12 +4,30 @@ require 'faker'
 db = SQLite3::Database.new("members.db")
 
 create_table_cmd = <<-SQL 
-	CREATE TABLE  members_list (
+	CREATE TABLE IF NOT EXISTS members_list (
 		id INTEGER PRIMARY KEY, 
 		name VARCHAR (255),
 		company VARCHAR (255), 
-		paid BOOLEAN
+		paid VARCHAR
 	)
 SQL
 
 db.execute(create_table_cmd)
+
+members_list = db.execute ("SELECT * FROM members_list")
+# puts members_list.class
+# p members_list
+
+def add_member(db, name, company, paid)
+	db.execute("INSERT INTO members_list (name, company, paid) VALUES (?, ?, ?)", [name, company, paid])
+end 	
+
+10.times do
+	add_member(db, Faker::Name.name, Faker::Company.name, "YES")
+end 	
+
+
+# members_list.each do |member|
+# 	puts "#{member['name']} works for #{member['company']}"
+# 	puts "status: #{member['paid']}"
+# end 	
