05ef4638dc2d87baead8abcac43be5d4a1747d18
Julia Greenwald <julia.greenwald@gmail.com>
2017-03-19 23:16:32 -0700

    add create table command and execute table

--- databases/awesome/awesome.rb
+++ databases/awesome/awesome.rb
@@ -3,3 +3,13 @@ require 'faker'
 
 db = SQLite3::Database.new("members.db")
 
+create_table_cmd = <<-SQL 
+	CREATE TABLE  members_list (
+		id INTEGER PRIMARY KEY, 
+		name VARCHAR (255),
+		company VARCHAR (255), 
+		paid BOOLEAN
+	)
+SQL
+
+db.execute(create_table_cmd)
