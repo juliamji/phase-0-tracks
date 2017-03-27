27befafca66266f7e12b07e9d5d9a824e6b3fdfc
Julia Greenwald <julia.greenwald@gmail.com>
2017-03-26 20:24:15 -0700

    modify index file and add application file manipulating an h2 and some divs

---
+++ web_dev/jquery/application.js
@@ -0,0 +1,11 @@
+$(function() {
+    $( "h2" ).text( "My Loves <3" );
+  });
+
+$(function () {
+	$( "#facebook" ).css( "border", "3px solid green" );
+});
+
+$(function(){
+	$("#fbnotes").css ("border", "3px solid black" );
+}); 
